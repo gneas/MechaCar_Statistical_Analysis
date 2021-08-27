2b6bc8880b390d4e9f712a4454188bba12396f5d
Gabriel <78886203+gneas@users.noreply.github.com>
2021-08-27 01:02:13 -0500

    Update challenge file and add images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,18 @@ lot_summary <- suspensioncoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean
 # Boxplot to examine Lot3 variance
 plt <- ggplot(suspensioncoil, aes(x=Manufacturing_Lot, y=PSI))
 plt + geom_boxplot()
+
+?t.test()
+
+# Perform t.test() to determine if psi across lots is statistically different from pop mean of 1,500psi.
+t.test(suspensioncoil$PSI,mu=1500)
+
+# Use subset() to create lots for use in t.test() against pop mean of 1,500psi.
+Lot1 <- subset(suspensioncoil,Manufacturing_Lot=="Lot1")
+Lot2 <- subset(suspensioncoil,Manufacturing_Lot=="Lot2")
+Lot3 <- subset(suspensioncoil,Manufacturing_Lot=="Lot3")
+
+# Perform t.test() on lots against pop mean of 1,500psi.
+t.test(Lot1$PSI, mu=1500)
+t.test(Lot2$PSI, mu=1500)
+t.test(Lot3$PSI, mu=1500)
